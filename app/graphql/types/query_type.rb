b7e7a25429f34b2357f70c2d774116154aab1ec8
Taylor Wu <taylorwu21@gmail.com>
2018-07-14 21:50:43 -0600

    finished all queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,10 +2,36 @@ class Types::QueryType < Types::BaseObject
   description "root query"
 
   field :authors, [AuthorType], null: true do
-    description "gets all authors"
+    description "Gets all authors"
+  end
+
+  field :author, AuthorType, null: true do
+    description "Find a author by id"
+    argument :id, ID, required: true
+  end
+
+  field :books, [BookType], null: true do
+    description "Get all Books"
+  end
+
+  field :book, BookType, null: true do
+    description "Find a book by id"
+    argument :id, ID, required: true
   end
 
   def authors
     Author.all
+  end
+
+  def author(id:)
+    Author.find(id)
+  end
+
+  def books
+    Book.all
+  end
+
+  def book(id:)
+    Book.find(id)
   end
 end
