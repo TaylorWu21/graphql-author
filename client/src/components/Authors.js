1e54dba7c41a326f053ae610c5244fb4d30bcc6e
Taylor Wu <taylorwu21@gmail.com>
2018-07-21 22:23:22 -0600

    authors.js completed

--- client/src/components/Authors.js
+++ client/src/components/Authors.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import {
+  Loader,
+  Segment,
+  Dimmer,
+  Card,
+  Container
+} from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+import query from '../queries/getAuthors';
 
 class Authors extends Component {
   render() {
     return (
-      <div>
-        AUTHORS
-      </div>
+      <Query
+        query={query}
+      >
+        {({ loading, error, data }) => {
+          if (loading) return(
+            <Segment>
+              <Dimmer active>
+                <Loader />
+              </Dimmer>
+            </Segment>
+          );
+
+          if (error) return <p>Error</p>;
+
+          return (
+            <Container>
+              <Card.Group centered>
+                {data.authors.map(({ id, name }) => (
+                <Card key={id} as={Link} to={`/authors/${id}`}>
+                  <Card.Content>
+                    <Card.Header>{name}</Card.Header>
+                  </Card.Content>
+                </Card>
+                ))}
+              </Card.Group>
+            </Container>
+          );
+        }}
+      </Query>
     )
   }
 }
