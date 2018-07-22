7d00559a439776796a2d4726e787e212999c72cb
Taylor Wu <taylorwu21@gmail.com>
2018-07-21 21:24:13 -0600

    setup deps in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import gql from "graphql-tag";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App';
+import 'semantic-ui-css/semantic.min.css';
+
+const client = new ApolloClient({
+  uri: "/graphql"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
+// How apollo can help us query data from the server
+client
+  .query({
+    query: gql `
+      {
+        authors {
+          name
+          books {
+            title
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
